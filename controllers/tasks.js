e6ee4fcd0b91624a641d464946cc56d1d545cb30
lulian666 <644139490@qq.com>
2022-12-23 15:24:02 +0800

    crud api with customed error handler

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,27 +1,48 @@
 const Task = require('../models/task')
+const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
 
-const getAllTasks = (req, res) => {
-    res.send('all items..')
-}
+const getAllTasks = asyncWrapper(async (req, res) => {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks })
+})
 
-const createTask = async (req, res) => {
+const createTask = asyncWrapper(async (req, res) => {
     const task = await Task.create(req.body)
-    res.status(201).json(task)
-}
+    res.status(201).json({ task })
+})
 
-const getTask = (req, res) => {
-    res.send({ id: req.params.id })
-}
+const getTask = asyncWrapper(async (req, res, next) => {
+    const { id } = req.params
+    const task = await Task.findOne({ _id: id })
+    if (!task) {
+        return next(createCustomError(`No task with id ${id}`, 404))
+        // return res.status(404).json({ msg: `No task with id ${id}` })
+    }
+    res.status(200).json({ task })
 
-const updateTask = (req, res) => {
-    console.log(req)
-    res.send('updateTask')
-}
+})
 
-const deleteTask = (req, res) => {
-    console.log(req)
-    res.send('deleteTask')
-}
+const updateTask = asyncWrapper(async (req, res, next) => {
+    const { id } = req.params
+    const task = await Task.findByIdAndUpdate({ _id: id }, req.body, {
+        new: true,
+        runValidators: true,
+    })
+    if (!task) {
+        return next(createCustomError(`No task with id ${id}`, 404))
+    }
+    res.status(200).json({ task })
+})
+
+const deleteTask = asyncWrapper(async (req, res, next) => {
+    const { id } = req.params
+    const task = await Task.findOneAndDelete({ _id: id })
+    if (!task) {
+        return next(createCustomError(`No task with id ${id}`, 404))
+    }
+    res.status(200).json({ task })
+})
 
 module.exports = {
     getAllTasks,
